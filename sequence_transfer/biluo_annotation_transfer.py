113d58793ba8e43f5ff38e5a753f1d9909e4eacb
laurent <l.bie@pangeanci.com>
2020-03-09 17:44:28 +0100

    Refactoring

--- sequence_transfer/biluo_annotation_transfer.py
+++ sequence_transfer/biluo_annotation_transfer.py
@@ -1,24 +1,69 @@
+from itertools import product
 from sequence_transfer.contextualized_sequence_transfer import ContextualizedTransfer
 from sequence_transfer.sequence_transfer import SequenceTransfer
 from sequence_transfer.sequence import BILUOAnnotationSequence
 from sequence_transfer.sequence import BILUOAnnotation
+from sequence_transfer.sequence_element import BILUOAnnotationAtom
+
+B = BILUOAnnotation.B
+I = BILUOAnnotation.I
+L = BILUOAnnotation.L
+U = BILUOAnnotation.U
+O = BILUOAnnotation.O
 
 
-B, I, L, I, O = BILUOAnnotation.CODES
 
 
 class BILUOAnnotationTransfer:
     def __init__(self, transfer: ContextualizedTransfer):
         self._transfer = transfer
 
-    def apply(self, sequence: BILUOAnnotationSequence, zip_target=False):
+    def apply(self, biluo_annotation_sequence: BILUOAnnotationSequence, zip_target=False):
+        transferred_annotations = []
+        for annotation in biluo_annotation_sequence:
 
-        for annotation in sequence:
-            # Apply is passed to the base class that transfer simple sequence
             transferred = SequenceTransfer.apply(self._transfer, annotation)
+            print(f"\n num letter {transferred.size}--------------")
+            atoms = annotation.materialize()[0].atoms
+            z = zip(*map(
+                lambda atom: list(map(
+                    lambda x: BILUOAnnotationAtom(x[0], x[1]),
+                    product(transfer_biluo(atom.code, transferred.size), [atom.tag])))
+                ,atoms))
 
-            # Extract sequence
-            a = self._transfer.source.context
+            z2 =  map(lambda atom_list:BILUOAnnotation(*atom_list), z)
+            for x in z2:
+                transferred_annotations.append(x)
+
+        print(transferred_annotations)
+        # return BILUOAnnotationSequence(0,
+        #
+        # )
+
+            # for i, y in enumerate(x):
+            #     print(f"-->level {i}:", y)
+
+            #print(*zip(*x))
+
+
+
+
+            # for atom in atoms:
+            #     code = atom.code
+            #     tag = atom.tag
+            #     t = map(
+            #         lambda x: BILUOAnnotationAtom(x[0], x[1]),
+            #         product(transfer_biluo(code, transferred.size), [tag]))
+            #     for x in t:
+            #         print(x.code, x.tag)
+            #     print("----------------")
+            # Apply is passed to the base class that transfer simple sequence
+
+
+            # # Extract sequence
+            # a = self._transfer.source.context
+
+
 
 
 class BILUOTransferException:
@@ -26,16 +71,22 @@ class BILUOTransferException:
         super().__init__("TransferException")
 
 
-def transfer_biluo(x: str, nb_slot: int) -> str:
-    if nb_slot == 0:
+def transfer_biluo(x: str, nb_slots: int) -> str:
+    if nb_slots == 0:
         raise BILUOTransferException()
     else:
-        if x == I:
-            return I*nb_slot
+        if x == I or x == O:
+            return x * nb_slots
         if x == B:
-            return B + I*(nb_slot - 1)
+            return B + I*(nb_slots - 1)
         elif x == L:
-            return I*(nb_slot - 1) + L
+            return I * (nb_slots - 1) + L
+        elif x == U:
+            if nb_slots == 1:
+                return U
+            else:
+                return B + I * (nb_slots - 2) + L
+
 
 
 

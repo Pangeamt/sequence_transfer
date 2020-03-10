336ef8d21c80dd2be8be4dfba224849458464004
Aleix <333aleix333@gmail.com>
2020-03-10 13:55:57 +0100

    Add remove html_entities_normalizer

---
+++ sequence_transfer/normalizer/remove_html_entities.py
@@ -0,0 +1,44 @@
+import html
+from typing import Tuple
+from sequence_transfer.sequence import Sequence
+from sequence_transfer.sequence_transfer import SequenceTransfer
+
+
+def is_entity(text: str, index: int) -> bool:
+    for i in range(index, index + 10):
+        if i < len(text):
+            if text[i] == ";":
+                return True, i
+            if text[i] == " ":
+                break
+    return False, -1
+
+
+def remove_html_entities(text: str) -> Tuple[str, SequenceTransfer]:
+    output = html.unescape(text)
+    j = 0
+    counter = 0
+    transfers = []
+
+    for i, char in enumerate(text):
+        if counter:
+            print(counter, char)
+            counter -= 1
+        elif char == "&":
+            is_html, fin = is_entity(text, i)
+            if is_html:
+                transfers.append((Sequence(i, fin + 1), Sequence(j)))
+                j += 1
+                counter = fin - i
+        else:
+            transfers.append((Sequence(i), Sequence(j)))
+            j += 1
+
+    print(transfers, output)
+
+    transfer = SequenceTransfer(
+        Sequence(0, len(text)),
+        Sequence(0, len(output)),
+        transfers
+    )
+    return output, transfer

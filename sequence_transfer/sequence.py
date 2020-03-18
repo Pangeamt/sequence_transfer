3e2ccd876db55a34a28ccb1f089de9879a017da9
LaurentBie <hloplol@gmail.com>
2020-03-18 16:24:39 +0100

    Refactor sequence.py

--- sequence_transfer/sequence.py
+++ sequence_transfer/sequence.py
@@ -1,6 +1,5 @@
 from typing import List, Tuple, Optional, Union, Any
 from colorama import Fore, Style
-from sequence_transfer.sequence_element import Token, Char
 
 
 #  Exceptions
@@ -187,6 +186,35 @@ class Sequence:
         self._context = context
 
     context = property(get_context, set_context)
+
+
+#  Sequence Element
+
+
+class Token:
+    def __init__(self, text: str):
+        self._text = text
+        if " " in text:
+            raise ValueError(f"token with whitespace `{text}`")
+
+    def get_text(self) -> str:
+        return self._text
+    text = property(get_text)
+
+    def __len__(self) -> int:
+        return len(self._text)
+
+
+class Char:
+    def __init__(self, text: str):
+        self._text = text
+
+    def get_text(self) -> str:
+        return self._text
+    text = property(get_text)
+
+    def __len__(self) -> int:
+        return len(self._text)
 
 
 #  Contexts
